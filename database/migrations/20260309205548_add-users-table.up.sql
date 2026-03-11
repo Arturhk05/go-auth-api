84680a765a2d23013cfc5abc84527006293f3f49
Arturhk05 <arturhandow@gmail.com>
2026-03-10 23:16:10 -0300

    feat(migrations): add indexes and updated_at trigger function on users table

--- database/migrations/20260309205548_add-users-table.up.sql
+++ database/migrations/20260309205548_add-users-table.up.sql
@@ -14,3 +14,17 @@ CREATE TABLE IF NOT EXISTS users (
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_users_created_at ON users(created_at);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users
+    FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();

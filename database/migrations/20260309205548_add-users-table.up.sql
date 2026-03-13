3b57ba858494cdd9b9d5b2258c23d5224628e4bf
Arturhk05 <arturhandow@gmail.com>
2026-03-13 14:04:00 -0300

    fix(migrations): remove IF NOT EXISTS from update_users_updated_at trigger

--- database/migrations/20260309205548_add-users-table.up.sql
+++ database/migrations/20260309205548_add-users-table.up.sql
@@ -25,5 +25,5 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER IF NOT EXISTS update_users_updated_at BEFORE UPDATE ON users
+CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users
     FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();

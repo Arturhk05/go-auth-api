d66b318ddd888a62367ae6b8014ab0b97e27ccb2
Arturhk05 <arturhandow@gmail.com>
2026-03-13 14:14:04 -0300

    feat(services): implement AuthService with Register method

---
+++ internal/services/auth_service.go
@@ -0,0 +1,49 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/arturhk05/go-auth-api/config"
+	"github.com/arturhk05/go-auth-api/internal/models"
+	"github.com/arturhk05/go-auth-api/internal/repositories"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService struct {
+	userRepo *repositories.UserRepository
+	cfg      *config.Config
+}
+
+func NewAuthService(userRepo *repositories.UserRepository, cfg *config.Config) *AuthService {
+	return &AuthService{
+		userRepo: userRepo,
+		cfg:      cfg,
+	}
+}
+
+func (s *AuthService) Register(password string, email string, username string) (uuid.UUID, error) {
+	_, err := s.userRepo.GetUserByEmail(email)
+	if err == nil {
+		return uuid.Nil, fmt.Errorf("user already exists")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), s.cfg.Security.BcryptCost)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to hash password")
+	}
+
+	user := &models.User{
+		ID:           uuid.New(),
+		Email:        email,
+		Username:     username,
+		PasswordHash: string(hashedPassword),
+	}
+
+	err = s.userRepo.CreateUser(user)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return user.ID, nil
+}
